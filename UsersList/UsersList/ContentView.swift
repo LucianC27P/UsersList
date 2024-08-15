235691df973b6041b1e18e8f29c3447b7431f155
Lucian Cristea <>
2024-08-15 12:44:17 +0300

    Start assignment

---
+++ UsersList/UsersList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UsersList
+//
+//  Created by Lucian Cristea on 15.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UsersList/UsersList/UsersListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersListApp.swift
+//  UsersList
+//
+//  Created by Lucian Cristea on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct UsersListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
